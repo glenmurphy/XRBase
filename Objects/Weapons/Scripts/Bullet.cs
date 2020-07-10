bb8cad0aca6bf20b81e6201ea89468183cb30ced
Glen Murphy <glen@glenmurphy.com>
2020-07-10 14:58:12 -0700

    Remove dependencies, add install instructions

--- Objects/Weapons/Scripts/Bullet.cs
+++ Objects/Weapons/Scripts/Bullet.cs
@@ -4,67 +4,71 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-  public float lifeTime = 1.4f;
-  private Vector3 lastPosition;
-  private RaycastHit hit;
-  private GameObject enemy;
-  private Rigidbody rb;
-  private bool inited = false;
+    public float lifeTime = 1.4f;
+    private Vector3 lastPosition;
+    private RaycastHit hit;
+    private GameObject enemy;
+    private Rigidbody rb;
+    private bool inited = false;
 
-  // Start is called before the first frame update
-  void Awake() {
-    if (!inited) Init(0);
-  }
-
-  // We might want to instantly advance the state of the bullet on instantiation; this is so 
-  // we can pretend it's been fired in the past for when we're doing big bursts
-  public void Init(float advanceTime) {
-    hit = new RaycastHit();
-    rb = GetComponent<Rigidbody>();
-
-    // Manually advance position by one frame because the bullet may have been created during
-    // an Update(), causing all kinds of physics havok. We could use the firing time of the
-    // weapon to position these more accurately as well.
-
-    lastPosition = transform.position;
-    transform.position = transform.position + rb.velocity * advanceTime;
-    Update();
-
-    Destroy(this.gameObject, lifeTime);
-  }
-
-  void Hit(RaycastHit hit) { 
-    Debug.Log("Collided with " + hit.collider.name);
-
-    if (hit.rigidbody) {
-      enemy = hit.collider.transform.root.gameObject;
-
-      Enemy enemyComponent = enemy.GetComponent<Enemy>();
-      if (enemyComponent)
-        enemyComponent.Hit(hit, GetComponent<Rigidbody>().velocity);
-      else
-        hit.rigidbody.AddForceAtPosition(GetComponent<Rigidbody>().velocity, hit.point);
+    // Start is called before the first frame update
+    void Awake() {
+        if (!inited) Init(0);
     }
-    
-    SmackPool.Instance.Create(hit.point,
-            Quaternion.AngleAxis(Random.Range(0f,360f), hit.normal) * 
-            Quaternion.LookRotation(hit.normal));
-    
-    Destroy(this.gameObject);
-  }
 
-  void CheckCollisions() {
-    if(Physics.Linecast(lastPosition, transform.position, out hit)) {
-      Hit(hit);
+    // We might want to instantly advance the state of the bullet on instantiation; this is so 
+    // we can pretend it's been fired in the past for when we're doing big bursts
+    public void Init(float advanceTime) {
+        hit = new RaycastHit();
+        rb = GetComponent<Rigidbody>();
+
+        // Manually advance position by one frame because the bullet may have been created during
+        // an Update(), causing all kinds of physics havok. We could use the firing time of the
+        // weapon to position these more accurately as well.
+
+        lastPosition = transform.position;
+        transform.position = transform.position + rb.velocity * advanceTime;
+        Update();
+
+        Destroy(this.gameObject, lifeTime);
     }
-  }
 
-  // Update is called once per frame
-  void Update() {
-    CheckCollisions();
+    void Hit(RaycastHit hit) { 
+        Debug.Log("Collided with " + hit.collider.name);
 
-    // Point bullet in the direction of travel
-    transform.forward = GetComponent<Rigidbody>().velocity;
-    lastPosition = transform.position;
-  }
+        if (hit.rigidbody) {
+            enemy = hit.collider.transform.root.gameObject;
+
+            /*
+            TODO: Move this into an interface so we don't have a dep on Enemy
+            if (Enemy) {
+                Enemy enemyComponent = enemy.GetComponent<Enemy>();
+            if (enemyComponent)
+                enemyComponent.Hit(hit, GetComponent<Rigidbody>().velocity);
+            else
+            */
+            hit.rigidbody.AddForceAtPosition(GetComponent<Rigidbody>().velocity, hit.point);
+        }
+        
+        SmackPool.Instance.Create(hit.point,
+                        Quaternion.AngleAxis(Random.Range(0f,360f), hit.normal) * 
+                        Quaternion.LookRotation(hit.normal));
+        
+        Destroy(this.gameObject);
+    }
+
+    void CheckCollisions() {
+        if(Physics.Linecast(lastPosition, transform.position, out hit)) {
+            Hit(hit);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        CheckCollisions();
+
+        // Point bullet in the direction of travel
+        transform.forward = GetComponent<Rigidbody>().velocity;
+        lastPosition = transform.position;
+    }
 }
--- README.md
+++ README.md
@@ -5,3 +5,13 @@ Features
 - Two-handed weapons
 - Magazines + slide reloading
 - Leaning over stuff while not destroying movement
+
+Installation
+- Start a new Unity project (I use URP)
+- Edit > Project Settings > XR Plugin Management > Install
+- Check the boxes for the headsets you want to use (SteamVR instructions TBD)
+- Install the XR Interaction Toolkit from the Package Manager (you might need to enable preview packages under "Advanced")
+- Install [Quick Outline](https://assetstore.unity.com/packages/tools/particles-effects/quick-outline-115488) from the Unity Asset store
+- Clone this repo into the /Assets/ folder, so you end up with /Assets/XRBase/
+- Drag the Player/Player prefab and the Outjects/Weapons/SMG prefabs into your scene
+- Be mad about Unity collision layers and how I haven't figured them out yet (I need to add a sample scene).
